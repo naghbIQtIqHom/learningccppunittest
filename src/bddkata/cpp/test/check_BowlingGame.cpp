d87846a5af8ba1a78543ec74b2f17b0a04df58c7
Kazuyuki Koishikawa <koi@kt.rim.or.jp>
2022-12-06 18:46:27 +0900

    p41

--- src/bddkata/cpp/test/check_BowlingGame.cpp
+++ src/bddkata/cpp/test/check_BowlingGame.cpp
@@ -29,11 +29,11 @@ Ensure( BowlingGame, AllOnes ) {
     assert_that(g.score(), is_equal_to(20));
 }
 
-// Ensure( BowlingGame, OneSpare ) {
-//     Game g;
-//     g.roll(5);
-//     g.roll(5); // spare
-//     g.roll(3);
-//     rollMany(g, 17, 0);
-//     assert_that(g.score(), is_equal_to(16));
-//}
+Ensure( BowlingGame, OneSpare ) {
+    Game g;
+    g.roll(5);
+    g.roll(5); // spare
+    g.roll(3);
+    rollMany(g, 17, 0);
+    assert_that(g.score(), is_equal_to(16));
+}
