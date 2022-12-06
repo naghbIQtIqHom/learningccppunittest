323d5a9236590403b2527cab3b60a9912a00c072
Kazuyuki Koishikawa <koi@kt.rim.or.jp>
2022-12-06 18:43:56 +0900

    p38

--- src/bddkata/cpp/src/BowlingGame.cpp
+++ src/bddkata/cpp/src/BowlingGame.cpp
@@ -10,6 +10,10 @@ int Game::score(void)
     int score = 0;
     for (int i = 0; i < sizeof(_rolls)/sizeof(*_rolls); i++)
     {
+        if (_rools[i] + _rolls[i+1] == 10)
+        {
+            score +=
+        }
         score += _rolls[i];
     }
     return score;
