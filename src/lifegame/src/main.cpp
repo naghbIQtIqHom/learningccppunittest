5a970ddeaf41cfbdf2a056ca6b24f6c2b526ff70
Kazuyuki Koishikawa <koi@kt.rim.or.jp>
2022-12-08 01:09:13 +0900

    begin lifegame

--- src/lifegame/src/main.cpp
+++ src/lifegame/src/main.cpp
@@ -20,8 +20,9 @@ int main(void)
     LifeGame g(79, 24); // x, y のセルの広さを定義する
     g.init(); // x,y にランダムにセルをばらまく
     for(int generation = 0; generation < 10; generation++) {
-        std::vector<bool> grid = g.next_generation(); // 配列に次世代セルを書き込む
+        std::vector<bool> grid = g.current(); // 現状のセルの状態を配列に書き込む
         display(79, 24, grid);
+        g.next_generation(); // 次世代セルを計算する
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     return 0;
